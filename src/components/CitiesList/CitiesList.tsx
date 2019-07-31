1e9280c2c67442cc0b5eafe35d93ce0a654717c0
mkinfrared <mkinfrared@gmail.com>
2019-07-31 13:51:51 +0300

    feat(#2): Added cities display
    * Added http request to '/api/catalog' on component mount
    * Displaying
    cities recieved from http request

--- src/components/CitiesList/CitiesList.tsx
+++ src/components/CitiesList/CitiesList.tsx
@@ -1,8 +1,36 @@
+import axios from "axios";
 import * as React from "react";
 
-class CitiesList extends React.Component {
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import { CitiesListState } from "components/CitiesList/CitiesList.type";
+import css from "components/CitiesList/CitiesList.module.scss";
+
+class CitiesList extends React.Component<{}, CitiesListState> {
+  public state: CitiesListState = {
+    cities: []
+  };
+
+  public componentDidMount() {
+    this.fetchCities();
+  }
+
+  private fetchCities = async () => {
+    const response = await axios.get("/api/catalog");
+    const cities = response.data;
+
+    this.setState({ cities });
+  };
+
   public render() {
-    return <div>CitiesList</div>;
+    const cities = this.state.cities.map(city => (
+      <Paper key={city.id}>
+        <Typography variant={"h6"}>{city.name}</Typography>
+      </Paper>
+    ));
+
+    return <div className={css.citiesList}>{cities}</div>;
   }
 }
 
